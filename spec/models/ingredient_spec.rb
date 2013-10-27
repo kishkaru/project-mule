7f986240d67bb3e4218d82981acabb54b71f325d
saasbook <saasbook@saasbook.(none)>
2013-10-27 04:42:02 -0700

    cleaned up ingredients model rspec to be closer to working

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Ingredient do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @item = Item.create({:name => 'Bongo Burger',
+          :price => 7.99,
+          :quantity => 30,
+          :expires_at => '08/27/2013 10:45 AM'})
+    @ingredient = Ingredient.create(:name => 'tomato')
+    @ingredient.items << @item
+  end
+  describe 'adding an ingredient' do
+    Ingredient.find(:all).include?(@ingredient) == true
+  end
+
+  describe 'deleting an ingredient' do
+    Ingredient.destroy(@ingredient)
+    Ingredient.find_by_name('tomato').should == nil
+  end
+
+  describe 'edit an ingredient' do
+    @ingredient.name.should == 'tomato'
+    @ingredient.name= 'potato'
+    @ingredient.name.should == 'potato'
+  end
+
+  describe 'reading an ingredient' do
+    Ingredient.find_by_name('tomato').should == @ingredient
+  end
 end
