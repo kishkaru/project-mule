6bc791d9e6255cdd2a65b9418877017906fa2400
saasbook <saasbook@saasbook.(none)>
2013-10-27 05:23:07 -0700

    Changed describes to its

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,29 +2,27 @@ require 'spec_helper'
 
 describe Ingredient do
   before(:each) do
-    @item = Item.create({:name => 'Bongo Burger',
-          :price => 7.99,
-          :quantity => 30,
-          :expires_at => '08/27/2013 10:45 AM'})
+    @item = Item.create(:name => 'Bongo Burger', :price => 7.99, :quantity => 30, :expires_at => '08/27/2013 10:45 AM')
     @ingredient = Ingredient.create(:name => 'tomato')
     @ingredient.items << @item
   end
-  describe 'adding an ingredient' do
-    Ingredient.find(:all).include?(@ingredient) == true
+
+  it 'adding an ingredient' do
+    Ingredient.all.include?(@ingredient) == true
   end
 
-  describe 'deleting an ingredient' do
-    Ingredient.destroy(@ingredient)
+  it 'deleting an ingredient' do
+    @ingredient.destroy
     Ingredient.find_by_name('tomato').should == nil
   end
 
-  describe 'edit an ingredient' do
+  it 'edit an ingredient' do
     @ingredient.name.should == 'tomato'
     @ingredient.name= 'potato'
     @ingredient.name.should == 'potato'
   end
 
-  describe 'reading an ingredient' do
+  it 'reading an ingredient' do
     Ingredient.find_by_name('tomato').should == @ingredient
   end
 end
