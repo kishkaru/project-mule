4fcad27e2797a9ff11968824f24a86abb5c3a1ac
Ryan Quan <ryanhquan@gmail.com>
2013-11-05 21:54:20 -0800

    Changed UsersHelper#update_user_attributes with simpler logic (.present?)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,8 @@ module UsersHelper
   # Updates USER_TO_UPDATE using the values in params[:user]. Returns true if save is successful
   def update_user_attributes(user_to_update)
     admin_updateable_attributes.each do |attr|
-      if params[:user][attr]
-        user_to_update.send("#{attr}=", params[:user][attr]) unless ((attr == :password || attr == :password_confirmation) && params[:user][attr] == "")
+      if params[:user][attr].present?
+        user_to_update.send("#{attr}=", params[:user][attr])
       end
     end
     user_to_update.save
