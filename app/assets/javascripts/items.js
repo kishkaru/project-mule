09cbb4fb8e2ab8097c4a639147f1b442d8a33e1e
Kishan Karunaratne <kishan@karu.io>
2013-11-20 04:46:36 -0800

    working jquery

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,32 +1,36 @@
+// Set up the UI/UX for the ships screens.  This object sets up all the functionality we need to:
+//  1.  Bind to the "click" event of the "#addButton" on the modal form.
+//  2.  Append data from the modal form to the Pilots table.
+//  3.  Hide the modal form when the user is done entering data.
+//
+// If any other events need to be wired up, the init function would be the place to put them.
 var ingredientFieldsUI = {
     init: function() {
-        $('#addButton').on('click', function() {
+        // Configuration for the jQuery validator plugin:
+        // Set the error messages to appear under the element that has the error.  By default, the
+        // errors appear in the all-too-familiar bulleted-list.
+        // Other configuration options can be seen here:  https://github.com/victorjonsson/jQuery-Form-Validator
+        var validationSettings = {
+            errorMessagePosition : 'element'
+        };
+
+        $('#addButton').on('click', function(e) {
+            // If the form validation on our Pilots modal "form" fails, stop everything and prompt the user
+            // to fix the issues.
+
             formHandler.appendFields();
             formHandler.hideForm();
         });
     }
 };
 
-var formHandler = {
-    // Public method for adding a new row to the table.
-    appendFields: function() {
-        // Get a handle on all the input fields in the form and detach them from the DOM (we will attach them later).
-        var inputFields = $(cfg.formId + ' ' + cfg.inputFieldClassSelector);
-        inputFields.detach();
-
-        // Build the row and add it to the end of the table.
-        rowBuilder.addRow(cfg.getTBodySelector(), inputFields);
-
-        // Add the "Remove" link to the last cell.
-        rowBuilder.link.appendTo($('tr:last td:last'));
-    },
-
-    // Public method for hiding the data entry fields.
-    hideForm: function() {
-        $(cfg.formId).modal('hide');
-    }
-};
-
+// Configuration for the Add/Edit Ship screen's functionality:
+//  formId:  The ID of the <FORM> that contains the input fields that need to be captured and appended to the table of Pilots.
+//  tableId:  The ID of the <TABLE> that represents the Pilots assigned to fly a particular ship.
+//  inputFieldClassSelector:  The CSS class that is assigned to all the data entry/input fields that need to be collected
+//      for appending to th Pilots table (and ultimately for saving to the database).
+//  getTBodySelector:  A VERY simple method that concatenates the cfg.tableId and " tbody" to build the selector we need
+//      to identify the <TABLE> where we'll be appending rows.
 var cfg = {
     formId: '#new-ingredient-fields',
     tableId: '#ingredients-table',
@@ -36,6 +40,29 @@ var cfg = {
     }
 };
 
+// Provides functionality to append new rows to the Pilots table and hide the modal form for adding new Pilots.
+// NOTE:  The "appendFields" function depends on the rowBuilder to handle building the HTML for the new row.
+var formHandler = {
+    // Public method for adding a new row to the table.
+    appendFields: function () {
+        // Get a handle on all the input fields in the form and detach them from the DOM (we'll attach them later).
+        var inputFields = $(cfg.formId + ' ' + cfg.inputFieldClassSelector);
+        inputFields.detach();
+
+        // Build the row and add it to the end of the table.
+        rowBuilder.addRow(cfg.getTBodySelector(), inputFields);
+
+        // Add the "Remove" link to the last cell.
+        rowBuilder.link.clone().appendTo($('tr:last td:last'));
+    },
+
+    // Public method for hiding the data entry fields.
+    hideForm: function() {
+        $(cfg.formId).modal('hide');
+    }
+};
+
+// Provides functionality for building the HTML that represents a new <TR> for the Pilots table.
 var rowBuilder = function() {
     // Private property that define the default <TR> element text.
     var row = $('<tr>', { class: 'fields' });
@@ -44,7 +71,7 @@ var rowBuilder = function() {
     var link = $('<a>', {
         href: '#',
         onclick: 'remove_fields(this); return false;',
-        title: 'Delete this Ingredient.'
+        title: 'Delete this Pilot.'
     }).append($('<i>', { class: 'icon-remove' }));
 
     // A private method for building a <TR> w/the required data.
@@ -53,8 +80,9 @@ var rowBuilder = function() {
 
         $(fields).map(function() {
             $(this).removeAttr('class');
-            return $('<td/>').append($(this));
-        }).appendTo(newRow);
+            var td = $('<td/>').append($(this));
+            td.appendTo(newRow);
+        });
 
         return newRow;
     }
@@ -71,3 +99,12 @@ var rowBuilder = function() {
         link: link
     }
 }();
+
+function name() {
+    var name = $('#name');
+    var item_name = $('#item_name');
+
+    name.keyup(function () {
+        item_name.text(name.val());
+    })
+}
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
+
   def link_to_remove_fields(name, f, options = {})
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", options)
   end
 
   def link_to_add_fields(name, f, association, options = {})
     new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, :child_index => "new_#{ association }") do |builder|
+    fields = f.fields_for(association, new_object, :child_index => "new_#{ association }", :onsubmit => "return $(this.)validate();") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
 
