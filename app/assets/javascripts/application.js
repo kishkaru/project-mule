a24c7840194082af6a10938c2641c8472477a694
ghear <ryanhigs@gmail.com>
2013-11-20 12:18:42 -0800

    cart responding to quantity adding and price adding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,15 +13,47 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-$( document ).ready( function() {
-	$('.plus').click(function() {
-		var row = this.parentNode.parentNode.id;
-		var t = $("#" + row + ".qty");
-		alert(t);
+$(document).ready(function() {
+	$("button").click(function() {
+		//if plus or minus
+		var row_id = this.parentNode.parentNode.id;
+		var current_quantity = get_quantity_value(get_quantity(row_id));
+		var current_price_value = get_price_value(get_price_object(row_id), current_quantity);
+		//var symbol = this.className;
+		//alert(symbol);
+		alter_quantity(row_id, current_quantity);
+		var new_quantity = get_quantity_value(get_quantity(row_id));
+		alter_price(row_id, current_price_value, new_quantity);
 	});
 
-	$('.minus').click( function() {
-		var row = this.parentNode.parentNode.id;
-		$("#" + row + ".qty").value -= 1;
-	});
+	var get_quantity = function(row_id) {
+		return $("#" + row_id).children(".qty");
+	};
+
+	var get_quantity_value = function(quantity) {
+		return Number(quantity.html());
+	};
+
+	var get_price_object = function(row_id) {
+		return $("#" + row_id).children(".price");
+	};
+
+	var get_price_value = function(price_object, quantity) {
+		return Number(price_object.html()) / quantity;
+	}
+
+	var alter_quantity = function(row_id, current_quantity) {
+		var quantity = get_quantity(row_id);
+		//if symbol == 'plus':
+		var new_quantity_value = current_quantity + 1;
+		//else:
+		//	var new_quantity_value = current_quantity - 1;
+		quantity.html(new_quantity_value);
+	};
+
+	var alter_price = function(row_id, current_price_value, quantity_value) {
+		var price = get_price_object(row_id);
+		var new_price_value = current_price_value * (quantity_value + 1);
+		price.html(new_price_value);
+	};
 });
