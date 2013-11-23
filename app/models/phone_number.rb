59ba761492d29fb7ad540cfb76164482651163a0
Ryan Quan <ryanhquan@gmail.com>
2013-11-22 18:03:55 -0800

    Added phone number validations for :area and :number. Added class method for PhoneNumber to create an instance given a number of the format from the jquery phone number form helper and corresponding RSpec

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,5 +1,28 @@
 class PhoneNumber < ActiveRecord::Base
+	validates :area, format: {with: /^\d{3}$/,
+							message: "Area code must be 3 digits"}
+	validate :number, format: {with: /^\d{3}-\d{4}$/,
+							message: "Phone number must be of the form 'ddd-dddd'"}
+
     belongs_to :vendor
     belongs_to :user
     attr_accessible :area, :country, :extension, :number
+
+    # creates and returns a phoneNumber instance given NUMBER
+    # with the format of "+d{2,3} (ddd) ddd-dddd". Returns nil if
+    # NUMBER is of an invalid format
+    def self.createPhoneNumber(number)
+    	format = /^\+\d{1,2} \(\d{3}\) \d{3}-\d{4}$/
+    	if format.match(number)
+    		fields = number.split(" ")
+    		tempCountry = fields[0][1,2]
+    		tempArea = fields[1][1,3]
+    		tempNumber = fields[2][0,8]
+    		return PhoneNumber.create(:country => tempCountry,
+    									:area => tempArea,
+    									:number => tempNumber)
+    	else
+    		return nil
+    	end
+    end
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe PhoneNumber do
-    pending "add some examples to (or delete) #{__FILE__}"
+
+    describe "createPhoneNumber method" do
+
+    	it "should return nil if given an invalid number format" do
+    		num = "+2(231) 123-2412"
+    		result = PhoneNumber.createPhoneNumber(num)
+    		result.should eq(nil)
+    	end
+
+    	it "should return a phone number instance if given a valid number format" do
+    		num = "+1 (123) 123-2412"
+    		result = PhoneNumber.createPhoneNumber(num)
+    		result.country.should eq("1")
+    		result.area.should eq("123")
+    		result.number.should eq("123-2412")
+    	end
+    end
 end
