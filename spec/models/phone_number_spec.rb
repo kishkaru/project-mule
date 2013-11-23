d2ab8c0222c7020f47a60063e793d469400bc9ee
Ryan Quan <ryanhquan@gmail.com>
2013-11-23 02:05:22 -0800

    Added RSpec test for phone number model validations

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -40,4 +40,46 @@ describe PhoneNumber do
 
     end
 
+    describe "validations" do
+
+        describe "should return true for country code" do
+
+            it "of length 1" do
+                phone = PhoneNumber.new({:area => "415", :country => "1", :extension => "434", :number => "123-4567"})
+                phone.valid?.should eq(true)
+            end
+
+            it "of length 2" do
+                phone = PhoneNumber.new({:area => "415", :country => "13", :extension => "434", :number => "123-4567"})
+                phone.valid?.should eq(true)
+            end
+
+        end
+
+        it "should return false for country code of length 3" do
+            phone = PhoneNumber.new({:area => "415", :country => "133", :extension => "434", :number => "123-4567"})
+            phone.valid?.should eq(false)
+        end
+
+        it "should return true for area code of legnth 3" do
+            phone = PhoneNumber.new({:area => "415", :country => "1", :extension => "434", :number => "123-4567"})
+            phone.valid?.should eq(true)
+        end
+
+        it "should return false for area code of length 4" do
+            phone = PhoneNumber.new({:area => "4153", :country => "1", :extension => "434", :number => "123-4567"})
+            phone.valid?.should eq(false)
+        end
+
+        it "should return true for number of format ddd-dddd" do
+            phone = PhoneNumber.new({:area => "415", :country => "1", :extension => "434", :number => "123-4567"})
+            phone.valid?.should eq(true)
+        end
+
+        it "should return false for area code of other format" do
+            phone = PhoneNumber.new({:area => "4153", :country => "1", :extension => "434", :number => "1234567"})
+            phone.valid?.should eq(false)
+        end
+    end
+
 end
