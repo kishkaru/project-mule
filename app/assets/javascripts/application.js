f65fe26317eadbcd726e904882b3f4c0e57d27bc
Ryan Quan <ryanhquan@gmail.com>
2013-11-23 15:49:18 -0800

    Added a spinner for cart when waiting for ajax request

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,102 +15,59 @@
 //= require_tree .
 $(document).ready(function() {
 
-	var get_quantity = function(row_id) {
-		return $("#" + row_id).children(".qty");
-	};
-
-	var get_quantity_value = function(quantity) {
-		return Number(quantity.html());
-	};
-
-	var get_price_object = function(row_id) {
-		return $("#" + row_id).children(".price");
-	};
-
-	var get_price_value = function(price_object) {
-		return Number(price_object.html());
-	};
-
-	var get_subtotal = function() {
-		return $('#subtotal');
-	};
-
-	var get_tax_total = function() {
-		return $('#tax-total');
-	};
-
-	var get_total = function() {
-		return $('#total');
-	}
-
-	var update_quantity = function(row_id, current_quantity, symbol) {
-		var quantity = get_quantity(row_id);
-		if (symbol == 'plus qty-buttons') {
-			var new_quantity_value = current_quantity + 1;
-		} else {
-			var new_quantity_value = current_quantity - 1;
-		}
-		quantity.html(new_quantity_value);
-	};
-
-	var update_subtotal = function(price_value, symbol) {
-		var subtotal = get_subtotal();
-		var subtotal_value = Number(subtotal.html());
-		if (symbol == 'plus qty-buttons') {
-			subtotal.html(subtotal_value + price_value);
-		} else {
-			subtotal.html(subtotal_value - price_value);
-		}
-	};
-
-	var update_tax = function() {
-		var tax_pct_str = $("#tax-pct").html();
-		tax_pct_str = tax_pct_str.substring(0, tax_pct_str.length - 1);
-		var tax_pct_value = Number(tax_pct_str);
-		var new_tax_total = tax_pct_value * Number(get_subtotal().html()) / 100.0;
-		get_tax_total().html(new_tax_total);
-	};
-
-	var update_total = function() {
-		get_total().html(Number(get_subtotal().html()) + Number(get_tax_total().html()));
+	var opts = {
+	  lines: 13, // The number of lines to draw
+	  length: 10, // The length of each line
+	  width: 5, // The line thickness
+	  radius: 15, // The radius of the inner circle
+	  corners: 1, // Corner roundness (0..1)
+	  rotate: 0, // The rotation offset
+	  direction: 1, // 1: clockwise, -1: counterclockwise
+	  color: '#000', // #rgb or #rrggbb or array of colors
+	  speed: 1, // Rounds per second
+	  trail: 60, // Afterglow percentage
+	  shadow: false, // Whether to render a shadow
+	  hwaccel: false, // Whether to use hardware acceleration
+	  className: 'spinner', // The CSS class to assign to the spinner
+	  zIndex: 2e9, // The z-index (defaults to 2000000000)
+	  top: 'auto', // Top position relative to parent in px
+	  left: 'auto' // Left position relative to parent in px
 	};
 
 	$("#cart_link").click( function () {
+		var target = document.getElementById('spinner-cart-center');
+		$("#cart-modal-table").html("");
+		var spinner = new Spinner(opts).spin(target);
 		$.ajax({type: "GET",
 			url: "/cart/",
 			success: function(data) {
-				$("#cart-modal-body").html(data);
-				/*$("button.qty-buttons").click(function() {
-					//if plus or minus
-					var row_id = this.parentNode.parentNode.id;
-					var current_quantity = get_quantity_value(get_quantity(row_id));
-					var price_value = get_price_value(get_price_object(row_id));
-					var symbol = this.className;
-					update_quantity(row_id, current_quantity, symbol);
-					update_subtotal(price_value, symbol);
-					update_tax();
-					update_total();
-				});*/
+				spinner.spin();
+				$("#cart-modal-table").html(data);
 				bind_qty_buttons();
 			}});
 	});
 
 	var bind_qty_buttons = function() {
+		var target = document.getElementById('spinner-cart-center');
 		$("button.qty-buttons.plus").click(function() {
+			var spinner = new Spinner(opts).spin(target);
 			var row_id = this.parentNode.parentNode.id;
 			$.ajax({type: "POST",
 				url: "/add_item/" + row_id + "/",
 				success: function(data) {
-					$("#cart-modal-body").html(data);
+					spinner.spin();
+					$("#cart-modal-table").html(data);
 					bind_qty_buttons();
 				}});
 		});
 		$("button.qty-buttons.minus").click(function() {
+			var spinner = new Spinner(opts).spin(target);
 			var row_id = this.parentNode.parentNode.id;
 			$.ajax({type: "POST",
 				url: "/minus_item/"  + row_id + "/",
 				success: function(data) {
-					$("#cart-modal-body").html(data);
+					spinner.spin();
+					$("#cart-modal-table").html(data);
 					bind_qty_buttons();
 				}});
 		});
