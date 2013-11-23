61d9bf0bfe5768e5cc9ce2012b0e4e488d15f089
Ryan Quan <ryanhquan@gmail.com>
2013-11-23 01:26:27 -0800

    Fixed validations for phone number to work as specified in last commit

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,18 +1,26 @@
 class PhoneNumber < ActiveRecord::Base
 
 	validates :area, format: {with: /^\d{3}$/,
-							message: "Area code must be 3 digits",
-                            if: Proc.new { |p| p.number || p.country }}
+							message: "code must be 3 digits"}, :allow_blank => true
 	validates :number, format: {with: /^\d{3}-\d{4}$/,
-							message: "Phone number must be of the form 'ddd-dddd'",
-                            if: Proc.new { |p| p.area || p.country }}
+							message: "number must be of the form 'ddd-dddd'"}, :allow_blank => true
     validates :country, format: {with: /^\d{1,2}$/,
-                            message: "Country code must be 1-2 digits",
-                            if: Proc.new { |p| p.number || p.area }}
+                            message: "country code must be 1-2 digits"}, :allow_blank => true
+    validate :area_number
 
     belongs_to :vendor
     belongs_to :user
     attr_accessible :area, :country, :extension, :number
+
+    # Validation that if area or number is present, the other must also be present
+    def area_number
+        if (area.present? && !number.present?)
+            errors.add(:number, "required if area code present")
+        end
+        if (!area.present? && number.present?)
+            errors.add(:area, "code required if number present")
+        end
+    end
 
     # Makes and returns a new phoneNumber instance given NUMBER
     # with the format of "+d{2,3} (ddd) ddd-dddd". Returns nil if
