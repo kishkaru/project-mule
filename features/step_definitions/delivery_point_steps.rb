0ef20979d0b0b6a3254b48d434bd99e240b446c9
Ryan Quan <ryanhquan@gmail.com>
2013-11-25 00:09:56 -0800

    Removed some temp files

--- features/step_definitions/delivery_point_steps.rb
+++
@@ -1,7 +0,0 @@
-Given /the following delivyer_points exist/ do |items_table|
-    items_table.hashes.each do |item|
-        delivery_area = item.delete("delivery_area")
-        DeliveryPoint.create!(item)
-    end
-end
-
