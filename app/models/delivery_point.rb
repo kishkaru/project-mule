4e9cb4a943e5b3094e81ec424cc0bc50cf3ba4c7
Jon San Miguel <sanmiguelje@yahoo.com>
2013-12-08 09:15:14 -0800

    Add picup point to users and navbar

--- app/models/delivery_point.rb
+++ app/models/delivery_point.rb
@@ -3,10 +3,9 @@ class DeliveryPoint < ActiveRecord::Base
     attr_accessible :latitude, :longitude, :delivery_area, :address_attributes
     validates_presence_of :address
 
-    belongs_to :customer, :class_name => "User"
-    belongs_to :delivery_area
-
+    has_many :customers, :class_name => "User", :foreign_key => "pickup_point_id"
     has_one :address, as: :addressable, :dependent => :destroy
+    belongs_to :delivery_area
 
     accepts_nested_attributes_for :address, :update_only => true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131207112458) do
+ActiveRecord::Schema.define(:version => 20131208164507) do
 
   create_table "addresses", :force => true do |t|
     t.string   "line1"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(:version => 20131207112458) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "braintree_token"
+    t.integer  "pickup_point_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
