d99b6972247194c158da2b88aa9800c9c70992ba
Jon San Miguel <sanmiguelje@yahoo.com>
2013-12-07 23:14:55 -0800

    Beachstrap

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,29 +1,28 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+adjustContainer = _.debounce(function() {
+    var height = $('header').outerHeight();
+    $('#main').css('top', height);
+    $('#main').css('height', $(window).height() - height);
+}, 300);
 
-var adjustContainer = function() {
-	$('body').css('padding-top', $('.container').height());
-};
+$( function() {
 
-$(window).resize( function() {
-	adjustContainer();
-});
+    $(window).resize( function() {
+        adjustContainer();
+    });
 
-$(document).ready( function() {
 
-	$.backstretch([
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/AtgWWlEXUT/southpaw.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/R5TpmLfBVq/gck-chicken-bowl.jpeg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/iwURrl1yZZ/jablows-signature_pastrami_sandwich.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/nmfVUugfOu/IMG_0223.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/_ir1_g0wyS/IMG_0360.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/JkDT1ISGyS/haparamen.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/dTw6HYGFZt/IMG_0172.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
-		'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/VEbeenwBcI/IMG_0218.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A'
-		],{duration: 10000, fade: 750});
-});
+    $(window).load(function() {
+        adjustContainer();
+    });
 
-$(window).load(function() {
-	adjustContainer();
+    $.backstretch([
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/AtgWWlEXUT/southpaw.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/R5TpmLfBVq/gck-chicken-bowl.jpeg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/iwURrl1yZZ/jablows-signature_pastrami_sandwich.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/nmfVUugfOu/IMG_0223.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/_ir1_g0wyS/IMG_0360.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/JkDT1ISGyS/haparamen.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/dTw6HYGFZt/IMG_0172.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A',
+        'https://dl.dropboxusercontent.com/sh/qz66j1s91ael6z5/VEbeenwBcI/IMG_0218.jpg?token_hash=AAEp5tYrEFxJV0mLEoYb1I61RkPdEcPLMzl7M065WBMQ-A'
+        ],{duration: 10000, fade: 750});
 });
