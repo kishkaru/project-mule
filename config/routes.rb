d0d0479a5d86f239b5ac582490813d577e9bd4ec
Ryan Quan <ryanhquan@gmail.com>
2013-12-08 19:05:46 -0800

    Added pagination and order show fixes

--- config/routes.rb
+++ config/routes.rb
@@ -66,8 +66,6 @@ Projectmule::Application.routes.draw do
 
     post '/account/set_default_credit_card', :to => 'credit_cards#setDefault', :as => :set_default_credit_card
 
-    get '/account/orders', :to => 'orders#user_orders', :as => :user_orders
-
     delete '/destroy_credit_card/:id', :to => 'credit_cards#destroy', :as => :destroy_credit_card
 
     post '/cart/change_credit_card', :to => 'credit_cards#changeCreditCard', :as => :change_credit_card
